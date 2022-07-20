610d26bf8cd4e7cc69f74736af87415c565df238
Natalia Gatti <gattinati.ng@gmail.com>
2022-07-20 12:19:24 -0300

    Solution

--- lib/dictionary.rb
+++ lib/dictionary.rb
@@ -1,1 +1,12 @@
 # Escribe tu código del ejercicio en este archivo. Haz que las pruebas en test/dictionary_test.rb pasen
+class Dictionary
+
+  def initialize(array)
+    @array = array
+  end
+
+  def find_meaning(word)
+    item = @array.select { |item| item.word == word }
+    item.last.meaning
+  end
+end
